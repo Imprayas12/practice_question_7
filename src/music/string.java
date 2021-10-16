33c3c2c009675e96854201574f1da0dfaaa008b7
prayas gautam <prayas12gautam@gmail.com>
2021-10-16 21:46:36 +0530

    Initial commit

---
+++ src/music/string.java
@@ -0,0 +1,9 @@
+package music;
+
+public class string {
+    public static class Veena implements Playable{
+        public void play(){
+            System.out.println("Playing Veena");
+        }
+    }
+}
---
+++ src/music/wind.java
@@ -0,0 +1,9 @@
+package music;
+
+public class wind {
+    public static class Saxophone implements Playable{
+        public void play(){
+            System.out.println("Playing Saxophone");
+        }
+    }
+}
